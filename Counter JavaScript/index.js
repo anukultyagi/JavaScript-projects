893930ad0e14264e7ee6b7a466ee1af6cea5d146
Neeraj <117462156+NeerajFull@users.noreply.github.com>
2023-01-02 17:30:08 +0530

    fixed color issue

--- Counter JavaScript/index.js
+++ Counter JavaScript/index.js
@@ -5,25 +5,30 @@ const increaseBtn = document.querySelector('.increase')
 const counter = document.querySelector('.counter')
 
 let count = 0;
-
 counter.innerHTML = count;
+
 increaseBtn.addEventListener('click', () => {
-    counter.innerHTML = count + 1;
-    count += 1;
+    count++;
+    if(count > 0){
+        counter.style.color = "green";
+    }else if(count === 0){
+        counter.style.color = "black"
+    }
+    counter.innerHTML = count;
 
 })
 decreaseBtn.addEventListener('click', () => {
-    counter.innerHTML = count - 1;
-    count -= 1;
+    count--;
+    if(count < 0){
+        counter.style.color = "red";
+    }else if(count === 0){
+        counter.style.color = "black";
+    }
+    counter.innerHTML = count;
 
 })
 
 resetBtn.addEventListener('click', () => {
     counter.innerHTML = 0;
+    counter.style.color = "black";
 })
-
-if (counter.innertext > 0) {
-    counter.style.color = 'green'
-} else if (counter.innerHTML < 0) {
-    counter.style.color = 'red'
-}
